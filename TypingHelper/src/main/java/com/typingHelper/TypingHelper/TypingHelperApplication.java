62044b47a8d36ddf448b7854abe3746eab55778f
pillow010 <89251942+pillow010@users.noreply.github.com>
2023-01-15 17:08:31 +0700

    Add files via upload
    base line generated from pring.io

---
+++ TypingHelper/src/main/java/com/typingHelper/TypingHelper/TypingHelperApplication.java
@@ -0,0 +1,13 @@
+package com.typingHelper.TypingHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingHelperApplication.class, args);
+	}
+
+}
---
+++ TypingHelper/src/test/java/com/typingHelper/TypingHelper/TypingHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typingHelper.TypingHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
