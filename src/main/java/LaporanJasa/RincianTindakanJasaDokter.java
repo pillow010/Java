29010290e209c1b714740fc3f96fa43baf7625c0
Sat <9a06satria@gmail.com>
2023-02-06 09:46:55 +0700

    finally 2. RINCIAN TINDAKAN JASA DOKTER is done. just few touch up like adjust width, adjust cell type as is it currency, text, date etc. ill figure it out letter.

--- src/main/java/LaporanJasa/RincianTindakanJasaDokter.java
+++ src/main/java/LaporanJasa/RincianTindakanJasaDokter.java
@@ -25,67 +25,154 @@ public class RincianTindakanJasaDokter {
 
             sheet = workbook.getSheetAt(0);
             Sheet sheet2 = workbook.createSheet();
+
             workbook.setSheetName(1, "2. RINCIAN TINDAKAN JASA DOKTER");
             int lastColumn = sheet.getRow(0).getLastCellNum();
             int lastRow = sheet.getLastRowNum();
             System.out.println("Last Column: " + lastColumn);
             System.out.println("Last Row: "+lastRow);
+            for (int row =1;row<=lastRow;row++){
+                sheet2.createRow(row);
+            }
+//
+//            sheet.getRow(0).createCell(60).setCellValue("NAMA PASIEN");
+//            sheet.getRow(0).createCell(61).setCellValue("NORM");
 
-            sheet.getRow(0).createCell(60).setCellValue("NAMA PASIEN");
-            sheet.getRow(0).createCell(61).setCellValue("NORM");
-
-            for (int column = 0; column <= lastColumn - 1; column++) {
-                Cell cell = sheet.getRow(0).getCell(column);
-                String cellValue = cell.getStringCellValue();
-                int targetColumn = switch (cellValue) {
-                    case "NAMA_PASIEN" -> 60;
-                    case "NORM" -> 61;
-                    case "NO_REG" -> 62;
-                    case "KET_INST" -> 63;
-                    case "KET_SUB_INST" -> 64;
-                    case "KET_DTL_SUB_INST" -> 65;
-                    case "NAMA_DOKTER" -> 66;
-                    case "POSISI" -> 67;
-                    case "TGL_TINDAKAN" -> 68;
-                    case "NM_TINDAKAN" -> 69;
-                    case "RUANG_RAWAT" -> 70;
-                    case "PAKET_JAMINAN" -> 71;
-                    case "JASA_PELAYANAN_TARIF" -> 72;
-                    case "JASA_PELAYANAN_JAMIN" -> 73;
-                    case "JML_PENDAPATAN" -> 74;
-                    case "JML_PENERIMAAN_TUNAI" -> 75;
-                    case "JML_PENERIMAAN_PIUTANG" -> 76;
-                    case "JML_PENERIMAAN_JMN" -> 77;
-                    case "JML_KOREKSI" -> 78;
-                    case "JML_PAJAK" -> 79;
-                    case "JML_PENGURANG_JASA" -> 80;
-                    case "JML_PENGAMBILAN" -> 81;
-                    case "JML_NETTO" -> 82;
-                    default -> -1;
-                };
+            Row row = sheet2.getRow(0);
+            if (row == null) {
+                row = sheet2.createRow(0);
+            }
+            row.createCell(0).setCellValue("NAMA PASIEN");
+            row.createCell(1).setCellValue("NORM");
+            row.createCell(2).setCellValue("NO REG");
+            row.createCell(3).setCellValue("KET INST");
+            row.createCell(4).setCellValue("KET SUB INST");
+            row.createCell(5).setCellValue("KET DTL SUB INST");
+            row.createCell(6).setCellValue("NAMA DOKTER");
+            row.createCell(7).setCellValue("POSISI");
+            row.createCell(8).setCellValue("TGL TINDAKAN");
+            row.createCell(9).setCellValue("NM TINDAKAN");
+            row.createCell(10).setCellValue("RUANG RAWAT");
+            row.createCell(11).setCellValue("PAKET JAMINAN");
+            row.createCell(12).setCellValue("JASA PELAYANAN TARIF");
+            row.createCell(13).setCellValue("JASA PELAYANAN JAMIN");
+            row.createCell(14).setCellValue("JML PENDAPATAN");
+            row.createCell(15).setCellValue("JML PENERIMAAN TUNAI");
+            row.createCell(16).setCellValue("JML PENERIMAAN PIUTANG");
+            row.createCell(17).setCellValue("JML PENERIMAAN JMN");
+            row.createCell(18).setCellValue("JML KOREKSI");
+            row.createCell(19).setCellValue("JML PAJAK");
+            row.createCell(20).setCellValue("JML PENGURANG JASA");
+            row.createCell(21).setCellValue("JML PENGAMBILAN");
+            row.createCell(22).setCellValue("JML NETTO");
 
 
-                if (targetColumn != -1) {
-                    for (int i = 1; i <= lastRow; i++) {
-                        Cell targetCell = sheet.getRow(i).createCell(targetColumn);
-                        sheet.getRow(0).createCell(targetColumn).setCellValue(cellValue);
-                        if (sheet.getRow(i).getCell(column).getCellType() == CellType.STRING)
+//            for (int column = 0; column <= lastColumn - 1; column++) {
+//                Cell cell = sheet.getRow(0).getCell(column);
+//                String cellValue = cell.getStringCellValue();
+//                int targetColumn2 = switch (cellValue) {
+//                    case "NAMA_PASIEN" -> 0;
+//                    case "NORM" -> 1;
+//                    case "NO_REG" -> 2;
+//                    case "KET_INST" -> 3;
+//                    case "KET_SUB_INST" -> 4;
+//                    case "KET_DTL_SUB_INST" -> 5;
+//                    case "NAMA_DOKTER" -> 6;
+//                    case "POSISI" -> 7;
+//                    case "TGL_TINDAKAN" -> 8;
+//                    case "NM_TINDAKAN" -> 9;
+//                    case "RUANG_RAWAT" -> 10;
+//                    case "PAKET_JAMINAN" -> 11;
+//                    case "JASA_PELAYANAN_TARIF" -> 12;
+//                    case "JASA_PELAYANAN_JAMIN" -> 13;
+//                    case "JML_PENDAPATAN" -> 14;
+//                    case "JML_PENERIMAAN_TUNAI" -> 15;
+//                    case "JML_PENERIMAAN_PIUTANG" -> 16;
+//                    case "JML_PENERIMAAN_JMN" -> 17;
+//                    case "JML_KOREKSI" -> 18;
+//                    case "JML_PAJAK" -> 19;
+//                    case "JML_PENGURANG_JASA" -> 20;
+//                    case "JML_PENGAMBILAN" -> 21;
+//                    case "JML_NETTO" -> 22;
+//                    default -> -1;
+//                };
+//                System.out.println(targetColumn2);
 
-                        {
-                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getStringCellValue());
-                        } else
-                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getNumericCellValue());
+//                if (targetColumn2 != -1) {
+//                    for (int i = 1; i <= lastRow; i++) {
+//                        Cell targetCell = sheet2.createRow(i).createCell(targetColumn2);
+//                        if (sheet.getRow(i).getCell(column).getCellType() == CellType.STRING)
+//                        {
+//                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getStringCellValue());
+//                        } else {
+//                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getNumericCellValue());
+//
+//                        }
+//                    }
+//                }
 
-                    }
-                }
+//            for (int column = 0; column <= lastColumn - 1; column++) {
+//                Cell cell = sheet.getRow(0).getCell(column);
+//                String cellValue = cell.getStringCellValue();
+//                int targetColumn = switch (cellValue) {
+//                    case "NAMA_PASIEN" -> 60;
+//                    case "NORM" -> 61;
+//                    case "NO_REG" -> 62;
+//                    case "KET_INST" -> 63;
+//                    case "KET_SUB_INST" -> 64;
+//                    case "KET_DTL_SUB_INST" -> 65;
+//                    case "NAMA_DOKTER" -> 66;
+//                    case "POSISI" -> 67;
+//                    case "TGL_TINDAKAN" -> 68;
+//                    case "NM_TINDAKAN" -> 69;
+//                    case "RUANG_RAWAT" -> 70;
+//                    case "PAKET_JAMINAN" -> 71;
+//                    case "JASA_PELAYANAN_TARIF" -> 72;
+//                    case "JASA_PELAYANAN_JAMIN" -> 73;
+//                    case "JML_PENDAPATAN" -> 74;
+//                    case "JML_PENERIMAAN_TUNAI" -> 75;
+//                    case "JML_PENERIMAAN_PIUTANG" -> 76;
+//                    case "JML_PENERIMAAN_JMN" -> 77;
+//                    case "JML_KOREKSI" -> 78;
+//                    case "JML_PAJAK" -> 79;
+//                    case "JML_PENGURANG_JASA" -> 80;
+//                    case "JML_PENGAMBILAN" -> 81;
+//                    case "JML_NETTO" -> 82;
+//                    default -> -1;
+//                };
+//
+//
+//                if (targetColumn != -1) {
+//                    for (int i = 1; i <= lastRow; i++) {
+//                        Cell targetCell = sheet.getRow(i).createCell(targetColumn);
+//                        sheet.getRow(0).createCell(targetColumn).setCellValue(cellValue);
+//                        if (sheet.getRow(i).getCell(column).getCellType() == CellType.STRING)
+//
+//                        {
+//                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getStringCellValue());
+//                        } else
+//                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getNumericCellValue());
+//
+//                    }
+//                }
+
+
 
 //                beri nama A1 "NOREG
-                sheet.getRow(0).createCell(62).setCellValue("NOREG");
+//                sheet.getRow(0).createCell(62).setCellValue("NOREG");
 
-//              jika cell mengandung "KD_INST" concat jadi noreg
-                if (cell.getStringCellValue().equals("KD_INST")) {
+//
+
+
+
+//            }
+
+            for (int column = 0; column <= lastColumn - 1; column++) {
+//          jika cell mengandung "KD_INST" concat jadi noreg
+                Cell cell = sheet.getRow(0).getCell(column);
+                if (sheet.getRow(0).getCell(column).getStringCellValue().equals("KD_INST")) {
                     for (int i = 1; i <= lastRow; i++) {
-                        Cell noReg = sheet.getRow(i).createCell(62);
+                        Cell noReg = sheet2.getRow(i).createCell(2);
                         noReg.setCellValue(sheet.getRow(i).getCell(column).getStringCellValue() +
                                 sheet.getRow(i).getCell(column + 1).getStringCellValue() +
                                 sheet.getRow(i).getCell(column + 2).getStringCellValue() +
@@ -94,12 +181,48 @@ public class RincianTindakanJasaDokter {
                     }
                 }
 
+                String cellValue = cell.getStringCellValue();
+                int targetColumn2 = switch (cellValue) {
+                    case "NAMA_PASIEN" -> 0;
+                    case "NORM" -> 1;
+                    case "NO_REG" -> 2;
+                    case "KET_INST" -> 3;
+                    case "KET_SUB_INST" -> 4;
+                    case "KET_DTL_SUB_INST" -> 5;
+                    case "NAMA_DOKTER" -> 6;
+                    case "POSISI" -> 7;
+                    case "TGL_TINDAKAN" -> 8;
+                    case "NM_TINDAKAN" -> 9;
+                    case "RUANG_RAWAT" -> 10;
+                    case "PAKET_JAMINAN" -> 11;
+                    case "JASA_PELAYANAN_TARIF" -> 12;
+                    case "JASA_PELAYANAN_JAMIN" -> 13;
+                    case "JML_PENDAPATAN" -> 14;
+                    case "JML_PENERIMAAN_TUNAI" -> 15;
+                    case "JML_PENERIMAAN_PIUTANG" -> 16;
+                    case "JML_PENERIMAAN_JMN" -> 17;
+                    case "JML_KOREKSI" -> 18;
+                    case "JML_PAJAK" -> 19;
+                    case "JML_PENGURANG_JASA" -> 20;
+                    case "JML_PENGAMBILAN" -> 21;
+                    case "JML_NETTO" -> 22;
+                    default -> -1;
+                };
 
+                if (targetColumn2 != -1) {
+                    for (int i = 1; i <= lastRow; i++) {
+                        Cell targetCell = sheet2.getRow(i).createCell(targetColumn2);
+                        if (sheet.getRow(i).getCell(column).getCellType() == CellType.STRING)
+                        {
+                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getStringCellValue());
+                        } else {
+                            targetCell.setCellValue(sheet.getRow(i).getCell(column).getNumericCellValue());
+
+                        }
+                    }
+                }
 
             }
-//
-
-
 
 //            for (int i = 0; i < 50; i++) {
 //                sheet.shiftColumns(i + 1, lastColumn, -1);
